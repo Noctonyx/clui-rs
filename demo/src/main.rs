87690f1689f165dbac752c39b54809aee1ddcaa2
Shane Hyde <shane@noctonyx.com>
2021-12-14 11:12:17 +1100

    Triangle working

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,17 +1,19 @@
 use log::LevelFilter;
 use std::path::Path;
+use std::time::Instant;
 
 use rafx_api::{
     RafxApi, RafxBuffer, RafxBufferDef, RafxColorClearValue, RafxColorRenderTargetBinding,
     RafxCommandBuffer, RafxCommandBufferDef, RafxCommandPool, RafxCommandPoolDef,
-    RafxDescriptorElements, RafxDescriptorKey, RafxDescriptorSetArrayDef, RafxDescriptorUpdate,
-    RafxDeviceContext, RafxError, RafxFormat, RafxGlUniformMember, RafxGraphicsPipelineDef,
-    RafxLoadOp, RafxPipeline, RafxPrimitiveTopology, RafxQueue, RafxQueueType, RafxResourceState,
-    RafxResourceType, RafxResult, RafxRootSignature, RafxRootSignatureDef, RafxSampleCount,
-    RafxShader, RafxShaderPackage, RafxShaderPackageVulkan, RafxShaderResource, RafxShaderStageDef,
-    RafxShaderStageFlags, RafxShaderStageReflection, RafxStoreOp, RafxSwapchainColorSpace,
-    RafxSwapchainDef, RafxSwapchainHelper, RafxTextureBarrier, RafxVertexAttributeRate,
-    RafxVertexLayout, RafxVertexLayoutAttribute, RafxVertexLayoutBuffer,
+    RafxDescriptorElements, RafxDescriptorKey, RafxDescriptorSetArray, RafxDescriptorSetArrayDef,
+    RafxDescriptorUpdate, RafxDeviceContext, RafxError, RafxFormat, RafxGlUniformMember,
+    RafxGraphicsPipelineDef, RafxLoadOp, RafxPipeline, RafxPrimitiveTopology, RafxQueue,
+    RafxQueueType, RafxResourceState, RafxResourceType, RafxResult, RafxRootSignatureDef,
+    RafxSampleCount, RafxShader, RafxShaderPackage, RafxShaderPackageVulkan, RafxShaderResource,
+    RafxShaderStageDef, RafxShaderStageFlags, RafxShaderStageReflection, RafxStoreOp,
+    RafxSwapchainColorSpace, RafxSwapchainDef, RafxSwapchainHelper, RafxTextureBarrier,
+    RafxVertexAttributeRate, RafxVertexBufferBinding, RafxVertexLayout, RafxVertexLayoutAttribute,
+    RafxVertexLayoutBuffer,
 };
 use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
@@ -27,18 +29,20 @@ struct DemoGraphicsContext {
     device_context: RafxDeviceContext,
     swapchain_helper: RafxSwapchainHelper,
     graphics_queue: RafxQueue,
-    shader: RafxShader,
-    root_signature: RafxRootSignature,
-    vertex_layout: RafxVertexLayout,
+    //shader: RafxShader,
+    //root_signature: RafxRootSignature,
+    //vertex_layout: RafxVertexLayout,
     pipeline: RafxPipeline,
     command_pools: Vec<RafxCommandPool>,
     command_buffers: Vec<RafxCommandBuffer>,
     vertex_buffers: Vec<Option<RafxBuffer>>,
     uniform_buffers: Vec<Option<RafxBuffer>>,
+    descriptor_set_array: RafxDescriptorSetArray,
 }
 
 struct DemoApp {
     api: RafxApi,
+    start_time: Instant,
     graphics: Option<DemoGraphicsContext>,
 }
 
@@ -50,12 +54,16 @@ impl DemoApp {
 
         Ok(DemoApp {
             api,
+            start_time: std::time::Instant::now(),
             graphics: Some(graphics),
         })
     }
 
     fn update(&mut self, window: &Window) -> Result<winit::event_loop::ControlFlow, RafxError> {
-        let mut ctx = self.graphics.as_mut().unwrap();
+        let ctx = self.graphics.as_mut().unwrap();
+
+        let elapsed_seconds = self.start_time.elapsed().as_secs_f32();
+
         //
         // Acquire swapchain image
         //
@@ -69,8 +77,39 @@ impl DemoApp {
 
         let cmd_pool = &mut ctx.command_pools[presentable_frame.rotating_frame_index()];
         let cmd_buffer = &ctx.command_buffers[presentable_frame.rotating_frame_index()];
-        let vertex_buffer = &ctx.vertex_buffers[presentable_frame.rotating_frame_index()];
-        let uniform_buffer = &ctx.uniform_buffers[presentable_frame.rotating_frame_index()];
+
+        #[rustfmt::skip]
+            let vertex_data = [
+            0.0f32, 0.5, 1.0, 0.0, 0.0,
+            0.5 - (elapsed_seconds.cos() / 2. + 0.5), -0.5, 0.0, 1.0, 0.0,
+            -0.5 + (elapsed_seconds.cos() / 2. + 0.5), -0.5, 0.0, 0.0, 1.0,
+        ];
+
+        let color = (elapsed_seconds.cos() + 1.0) / 2.0;
+        let uniform_data = [color, 0.0, 1.0 - color, 1.0];
+
+        let vertex_buffer = ctx
+            .device_context
+            .create_buffer(&RafxBufferDef::for_staging_vertex_buffer_data(&vertex_data))?;
+        vertex_buffer.copy_to_host_visible_buffer(&vertex_data)?;
+
+        let uniform_buffer =
+            ctx.device_context
+                .create_buffer(&RafxBufferDef::for_staging_uniform_buffer_data(
+                    &uniform_data,
+                ))?;
+        uniform_buffer.copy_to_host_visible_buffer(&uniform_data)?;
+
+        ctx.descriptor_set_array
+            .update_descriptor_set(&[RafxDescriptorUpdate {
+                array_index: presentable_frame.rotating_frame_index() as u32,
+                descriptor_key: RafxDescriptorKey::Name("color"),
+                elements: RafxDescriptorElements {
+                    buffers: Some(&[&uniform_buffer]),
+                    ..Default::default()
+                },
+                ..Default::default()
+            }])?;
 
         cmd_pool.reset_command_pool()?;
         cmd_buffer.begin()?;
@@ -103,6 +142,19 @@ impl DemoApp {
 
         cmd_buffer.cmd_bind_pipeline(&ctx.pipeline)?;
 
+        cmd_buffer.cmd_bind_vertex_buffers(
+            0,
+            &[RafxVertexBufferBinding {
+                buffer: &vertex_buffer,
+                byte_offset: 0,
+            }],
+        )?;
+        cmd_buffer.cmd_bind_descriptor_set(
+            &ctx.descriptor_set_array,
+            presentable_frame.rotating_frame_index() as u32,
+        )?;
+        cmd_buffer.cmd_draw(3, 0)?;
+
         cmd_buffer.cmd_end_render_pass()?;
 
         cmd_buffer.cmd_resource_barrier(
@@ -114,6 +166,9 @@ impl DemoApp {
             )],
         )?;
         cmd_buffer.end()?;
+
+        ctx.vertex_buffers[presentable_frame.rotating_frame_index()] = Some(vertex_buffer);
+        ctx.uniform_buffers[presentable_frame.rotating_frame_index()] = Some(uniform_buffer);
 
         //
         // Present the image
@@ -178,7 +233,7 @@ fn main() {
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-        .with_title("Apiary Demo")
+        .with_title("ClUI Demo")
         .with_inner_size(winit::dpi::LogicalSize::new(1600, 900))
         .build(&event_loop)
         .unwrap();
@@ -232,15 +287,6 @@ fn create_graphics_context(
     let mut uniform_buffers: Vec<Option<RafxBuffer>> =
         Vec::with_capacity(swapchain_helper.image_count());
 
-    #[rustfmt::skip]
-        let vertex_data = [
-        0.0f32, 0.5, 1.0, 0.0, 0.0,
-        -0.5, -0.5, 0.0, 1.0, 0.0,
-        0.5, 0.5, 0.0, 0.0, 1.0,
-    ];
-
-    let uniform_data = [1.0f32, 0.0, 1.0, 1.0];
-
     for _ in 0..swapchain_helper.image_count() {
         let mut command_pool =
             graphics_queue.create_command_pool(&RafxCommandPoolDef { transient: true })?;
@@ -248,31 +294,12 @@ fn create_graphics_context(
         let command_buffer = command_pool.create_command_buffer(&RafxCommandBufferDef {
             is_secondary: false,
         })?;
-        /*
-               let vertex_buffer = device_context
-                   .create_buffer(&RafxBufferDef::for_staging_vertex_buffer_data(&vertex_data))?;
-               vertex_buffer.copy_to_host_visible_buffer(&vertex_data)?;
 
-               let uniform_buffer = device_context.create_buffer(
-                   &RafxBufferDef::for_staging_uniform_buffer_data(&uniform_data),
-               )?;
-               uniform_buffer.copy_to_host_visible_buffer(&uniform_data)?;
-
-        */
         command_pools.push(command_pool);
         command_buffers.push(command_buffer);
-        //vertex_buffers.push(vertex_buffer);
-        //uniform_buffers.push(uniform_buffer);
         vertex_buffers.push(None);
         uniform_buffers.push(None);
     }
-
-    //        let render_registry = RenderRegistryBuilder::default()
-    //          .register_render_phase::<OpaqueRenderPhase>("Opaque")
-    //        .build();
-
-    //        let mut resource_manager = ResourceManager::new(&device_context, &render_registry);
-    //      let resource_context = resource_manager.resource_context();
 
     let shader = create_shader(&device_context)?;
 
@@ -290,29 +317,13 @@ fn create_graphics_context(
     // pooling system around this. (Higher-level rafx crates provide this.) But they're small
     // and cheap. We need one per swapchain image.
     //
-    let mut descriptor_set_array =
+    let descriptor_set_array =
         device_context.create_descriptor_set_array(&RafxDescriptorSetArrayDef {
             set_index: 0,
             root_signature: &root_signature,
             array_length: swapchain_helper.image_count(), // One per swapchain image.
         })?;
 
-    /*
-    // Initialize them all at once here.. this can be done per-frame as well.
-    for i in 0..swapchain_helper.image_count() {
-        descriptor_set_array.update_descriptor_set(&[RafxDescriptorUpdate {
-            array_index: i as u32,
-            descriptor_key: RafxDescriptorKey::Name("color"),
-            elements: RafxDescriptorElements {
-                buffers: Some(&[&uniform_buffers[i].unwrap()]),
-                ..Default::default()
-            },
-            ..Default::default()
-        }])?;
-    }
-
-
-     */
     //
     // Now set up the pipeline. LOTS of things can be configured here, but aside from the vertex
     // layout most of it can be left as default.
@@ -357,14 +368,15 @@ fn create_graphics_context(
         device_context,
         swapchain_helper,
         graphics_queue,
-        shader,
-        root_signature,
-        vertex_layout,
+        //shader,
+        //root_signature,
+        //vertex_layout,
         pipeline,
         command_pools,
         command_buffers,
         vertex_buffers,
         uniform_buffers,
+        descriptor_set_array,
     })
 }
 
